9c6676ceda91acec7a2279d92d9f76a6105ff02f
RfanKD <rfan.kd@gmail.com>
2014-02-18 15:31:48 -0500

    dragging work ... now need dropping

--- Project App/src/com/example/acemenu/Level_One.java
+++ Project App/src/com/example/acemenu/Level_One.java
@@ -2,18 +2,48 @@ package com.example.acemenu;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.ClipData;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.view.DragEvent;
+import android.view.MotionEvent;
 import android.view.View;
+import android.view.View.DragShadowBuilder;
 import android.view.View.OnClickListener;
+import android.view.View.OnDragListener;
+import android.view.View.OnTouchListener;
+import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.RelativeLayout;
 
 public class Level_One extends Activity {
+	
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.level_one);
+		
+		findViewById(R.id.imageView1).setOnTouchListener(new MyTouchListener());
 	}
+	
+	
+	
+	private final class MyTouchListener implements OnTouchListener {
+		  public boolean onTouch(View view, MotionEvent motionEvent) {
+		    if (motionEvent.getAction() == MotionEvent.ACTION_DOWN) {
+		      ClipData data = ClipData.newPlainText("", "");
+		      DragShadowBuilder shadowBuilder = new View.DragShadowBuilder(view);
+		      view.startDrag(data, shadowBuilder, view, 0);
+		      view.setVisibility(View.INVISIBLE);
+		      return true;
+		    } else {
+		    	return false;
+		    }
+		  }
 
+	}
+	
+	
 }
