49f2aa1010db3cf3c78610bf21110efc58b05969
alisonvdw <alison.vanderwee@mail.mcgill.ca>
2014-02-22 17:32:03 -0500

    hey look, the app runs again.

--- App/gen/com/example/acememo/R.java
+++ App/gen/com/example/acememo/R.java
@@ -67,9 +67,9 @@ public final class R {
         public static final int app_name=0x7f060000;
         public static final int hello_world=0x7f060002;
         public static final int howToPlay=0x7f060004;
-        public static final int instr1=0x7f060004;
-        public static final int instr2=0x7f060005;
-        public static final int level=0x7f060006;
+        public static final int instr1=0x7f060005;
+        public static final int instr2=0x7f060006;
+        public static final int level=0x7f060007;
     }
     public static final class style {
         /** 
