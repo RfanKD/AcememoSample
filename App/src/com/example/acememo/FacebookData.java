73d1f97be61fb3a00bc3f019525cb58326f7e82a
Emir Aydin <emraydn12@gmail.com>
2014-03-20 05:43:02 -0400

    Facebook Data class

---
+++ App/src/com/example/acememo/FacebookData.java
@@ -0,0 +1,107 @@
+package com.example.acememo;
+
+import java.util.List;
+import java.util.Random;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import android.os.Bundle;
+import com.facebook.*;
+import com.facebook.Request.GraphUserListCallback;
+import com.facebook.model.*;
+
+public class FacebookData {
+
+	private final JSONArray result = new JSONArray();
+	private static final Random random = new Random();
+
+	
+	public FacebookData(int number) {
+		getFriends(number);
+	}
+	
+	private void getFriends(int howMany){
+		final int amount = howMany;
+		Session activeSession = Session.getActiveSession();
+		if(activeSession.getState().isOpened()){
+			Request friendRequest = Request.newMyFriendsRequest(activeSession, 
+					new GraphUserListCallback(){
+				@Override
+				public void onCompleted(List<GraphUser> users,
+						Response response) {
+					JSONArray data;
+					try {
+						data = (JSONArray) response.getGraphObject().getInnerJSONObject().get("data");
+						getLikesOfAFriend(data, amount);
+
+					} catch (JSONException e) {
+						e.printStackTrace();
+					}
+
+				}
+			});
+			Bundle params = new Bundle();
+			params.putString("fields", "id, first_name");
+			friendRequest.setParameters(params);
+			friendRequest.executeAsync();
+		}
+	}
+	
+	private static int getRandom(int max) {
+		int randomNumber = random.nextInt(max);
+		return randomNumber;
+	}
+
+	private void getLikesOfAFriend(JSONArray friends, int howMany) throws JSONException {
+		// Start constructing the resulting array
+		for (int i=0; i<howMany; i++) {
+			JSONObject f = (JSONObject) friends.get(getRandom(friends.length()));
+			addToResult((String) f.get("id"), (String) f.get("first_name"));
+		}
+		
+	}
+
+	private void addToResult(String uid, String fname) {
+		String fqlQuery = "SELECT page_id, name FROM page where page_id IN (SELECT page_id FROM page_fan WHERE uid=" + uid + ")";
+		Bundle params = new Bundle();
+		params.putString("q", fqlQuery);
+		final String userId = uid;
+		final String firstName = fname;
+		Session session = Session.getActiveSession();
+		Request request = new Request(session, 
+				"/fql", 
+				params, 
+				HttpMethod.GET, 
+				new Request.Callback(){ 
+			public void onCompleted(Response response) {
+				try {
+					JSONArray dataArray = (JSONArray) response.getGraphObject().getInnerJSONObject().get("data");
+					JSONObject data = (JSONObject) dataArray.get(getRandom(dataArray.length()));
+					String pid = (String) data.get("page_id");
+					String name = (String) data.get("name");
+					JSONObject j = new JSONObject();
+					j.put("personId", userId);
+					j.put("personName", firstName);
+					j.put("likeId", pid);
+					j.put("likeName", name);
+					result.put(j);
+					
+				} catch (JSONException e) {
+					e.printStackTrace();
+				}
+			}
+		});
+		Request.executeBatchAsync(request);
+
+	}
+	
+	// This should be called with some delay
+	public JSONArray getResult() {
+	      
+        return result;
+	}
+
+
+
+}
--- App/src/com/example/acememo/FacebookLogin.java
+++ App/src/com/example/acememo/FacebookLogin.java
@@ -1,40 +1,24 @@
 package com.example.acememo;
 
-import java.util.List;
-import java.util.Random;
-
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import com.example.acememo.R;
-
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.Button;
-import android.widget.TextView;
-import android.widget.Toast;
-
 import com.facebook.*;
-import com.facebook.Request.GraphUserListCallback;
 import com.facebook.model.*;
-
-
-public class FacebookLogin extends Activity{
+public class FacebookLogin extends Activity {
 
 	Button allow;
 	Button disallow;
-
-	private static final int MAX_FRIENDS = 10;
-	private String friendLikes = null;
-
+	
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.facebook_login);
 		Log.d("LogByEmir", "FacebookLogin Activity Started");
+		
+		final Context context = this;
 		// start Facebook Login
 		Session.openActiveSession(this, true, new Session.StatusCallback() {
 
@@ -50,9 +34,11 @@ public class FacebookLogin extends Activity{
 						public void onCompleted(GraphUser user, Response response) {
 							if (user != null) {
 								Log.d("LogByEmir", "request completed");
-								TextView welcome = (TextView) findViewById(R.id.facebookInstructions);
-								welcome.setText("Hello " + user.getName() + "!");
-								getFriends();
+								// User is logged in, forward to next activity
+								Intent GameLevel = new Intent(context,Game_Level.class);
+								startActivity(GameLevel);
+								finish();
+								
 							}
 						}
 					}).executeAsync();
@@ -61,78 +47,12 @@ public class FacebookLogin extends Activity{
 			}
 		});
 	}	
-
+	
 	@Override
 	public void onActivityResult(int requestCode, int resultCode, Intent data) {
 		super.onActivityResult(requestCode, resultCode, data);
 		Session.getActiveSession().onActivityResult(this, requestCode, resultCode, data);
 		Log.d("LogByEmir", "here in OnActivityResult");
-	}
-
-	private void getFriends(){
-		Session activeSession = Session.getActiveSession();
-		if(activeSession.getState().isOpened()){
-			Request friendRequest = Request.newMyFriendsRequest(activeSession, 
-					new GraphUserListCallback(){
-				@Override
-				public void onCompleted(List<GraphUser> users,
-						Response response) {
-					JSONArray data;
-					try {
-						data = (JSONArray) response.getGraphObject().getInnerJSONObject().get("data");
-						getLikesOfAFriend(data);
-					} catch (JSONException e) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
-					}
-
-				}
-			});
-			Bundle params = new Bundle();
-			params.putString("fields", "id, first_name, picture");
-			friendRequest.setParameters(params);
-			friendRequest.executeAsync();
-		}
-	}
-
-	private void getLikesOfAFriend(JSONArray friends) throws JSONException {
-		Random random = new Random();
-		int randomNumber = random.nextInt(friends.length());
-
-		JSONArray result = new JSONArray();
-		for (int i=0; i<MAX_FRIENDS; i++) {
-			JSONObject element = new JSONObject();
-			JSONObject f = (JSONObject) friends.get(randomNumber);
-			element.put("personId", (String) f.get("id"));
-			element.put("personName", (String) f.get("first_name"));
-			String picture = (String) ((JSONObject) ((JSONObject) f.get("picture")).get("data")).get("url");
-			element.put("personImage", picture.replace("https", "http"));
-//			Log.d("LikeLog", getFriendsLikes((String) f.get("id")));
-			friendLikes = null;
-			element.put("likeName", "xx");
-			element.put("likeImage", "yy");
-			result.put(element);
-		}
-		Log.d("LogByEmir", result.toString());
-	}
-
-	private String getFriendsLikes(String uid) {
-		Session session = Session.getActiveSession();
-		Request.Callback callback = new Request.Callback() {
-			@Override
-			public void onCompleted(Response response) {
-				// response should have the likes
-				friendLikes = response.toString();
-	        	Log.d("LikeLog", friendLikes.toString());
-			}
-		};
-		Request request = new Request(session, "me" + "/likes", null, HttpMethod.GET, callback);
-		RequestAsyncTask task = new RequestAsyncTask(request);
-		task.execute();
-		while (friendLikes==null) {
-			// do nothing
-		}
-		return friendLikes;
 	}
 
 
