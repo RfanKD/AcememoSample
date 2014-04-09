42891f1c3fee576f4694aeece6cd9d7dadd980be
alisonvdw <alison.vanderwee@mail.mcgill.ca>
2014-04-09 03:34:41 -0400

    play without faecbook works.

--- App/src/com/example/acememo/HardcodedJSON.java
+++ App/src/com/example/acememo/HardcodedJSON.java
@@ -1,5 +1,8 @@
 package com.example.acememo;
 
+import java.util.Arrays;
+import java.util.Collections;
+
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -28,10 +31,10 @@ public class HardcodedJSON {
 	
 	JSONObject[] jsonArray = new JSONObject[16];
 	
-	int[] itemDraw = {R.drawable.pizza, R.drawable.music, R.drawable.painting, R.drawable.biking, R.drawable.photography, R.drawable.lego, R.drawable.movies, R.drawable.soccer};
-	int[] peopleDraw = {R.drawable.emir, R.drawable.alison, R.drawable.raph, R.drawable.jim, R.drawable.miley, R.drawable.will};
-	String[] itemName = {"pizza", "music", "painting", "biking", "photography", "lego", "movies", "soccer"};
-	String[] personName = {"Emir", "Alison", "Raph", "Jim", "Miley", "Will"};
+	int[] itemDraw = {R.drawable.pizza, R.drawable.music, R.drawable.painting, R.drawable.biking, R.drawable.photography, R.drawable.lego, R.drawable.movies, R.drawable.soccer, R.drawable.apple, R.drawable.beach, R.drawable.chocolate, R.drawable.dog, R.drawable.fireworks, R.drawable.microsoft, R.drawable.money, R.drawable.oranges, R.drawable.plane, R.drawable.school, R.drawable.scuba, R.drawable.travel, R.drawable.yoga};
+	int[] peopleDraw = {R.drawable.emir, R.drawable.alison, R.drawable.raph, R.drawable.jim, R.drawable.miley, R.drawable.will, R.drawable.albert, R.drawable.ashton, R.drawable.diana, R.drawable.howie, R.drawable.kim, R.drawable.mark, R.drawable.mrbean, R.drawable.nelson, R.drawable.queen, R.drawable.shrek};
+	String[] itemName = {"pizza", "music", "painting", "biking", "photography", "lego", "movies", "soccer", "Apple", "beach", "chocolate", "dogs", "fireworks", "Microsoft", "money", "oranges", "airplanes", "school", "scuba diving", "travelling", "yoga"};
+	String[] personName = {"Emir", "Alison", "Raph", "Jim", "Miley", "Will", "Albert", "Ashton", "Diana", "Howie", "Kim", "Mark", "Mr.Bean", "Nelson", "The Queen", "Shrek"};
 	
 	public HardcodedJSON(int level){
 		setUpJSONS(level);
@@ -40,14 +43,20 @@ public class HardcodedJSON {
 	public void setUpJSONS(int level){
 		fillArray();
 		gameArray = new JSONArray();
+		
+		Integer[] peopleIndex = {0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15};
+		Collections.shuffle(Arrays.asList(peopleIndex));
+		
+		Integer[] likeIndex = {0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20};
+		Collections.shuffle(Arrays.asList(likeIndex));
 
 		for(int i=0; i<level; i++){
 			try {
-				jsonArray[i].put("personIdd", "00000000");
-				jsonArray[i].put("personName", personName[i % 6]);
-				jsonArray[i].put("personImage",  peopleDraw[i % 6]);
-				jsonArray[i].put("likeName", itemName[i % 8]);
-				jsonArray[i].put("likeImage", itemDraw[i % 8]);
+				jsonArray[i].put("personId", "00000000");
+				jsonArray[i].put("personName", personName[peopleIndex[i]]);
+				jsonArray[i].put("personImage",  peopleDraw[peopleIndex[i]]);
+				jsonArray[i].put("likeName", itemName[likeIndex[i]]);
+				jsonArray[i].put("likeImage", itemDraw[likeIndex[i]]);
 				
 				gameArray.put(i, jsonArray[i]);
 			} catch (JSONException e) {
